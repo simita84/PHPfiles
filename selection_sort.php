5967f4520c5781ac968c4f06b822ca948792b8f2
simi <simi.tresa.antony@gmail.com>
2015-01-30 11:57:24 -0800

    Selection Sort

--- selection_sort.php
+++ selection_sort.php
@@ -3,43 +3,51 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Selection Sort</title>
+	<link rel="stylesheet" 
+		  href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 </head>
-<body>
-	<?php $array= array(56,77,6,2,4,34,67,1,9);
-		var_dump($array);
-        $smallest_index;
-        $smallest;
- 
-		for ($i=0; $i <count($array) ; $i++) { 
-			 
-			 for($j=$i+1;$j<count($array);$j++){
-			 	
-			 	if($array[$j]<$array[$i]){
-			 		$smallest=$array[$j];
-			 		//echo "smallest=".$smallest;
-			 		$smallest_index=$j;
-			 		//echo "index= ".$smallest_index;
-			 		 
-			 	}
+<body class="container">
+	<h1 class="page-header">Selection Sort</h1>
+	<div> 
+		<h2>Initial Array</h2>
+		<?php  
+		  $array=array();
+		   for ($index=0; $index < 1000; $index++) { 
+			 	 $number=rand(1,1000);
+			 	 array_push($array,  $number) ;
+		   }
+			echo count($array);
+			//var_dump($array);
+			$time_before=microtime();
+			for ($i=0; $i <count($array) ; $i++) { 
+				$smallest=$array[$i];
+				$smallest_index=$i;
+				for ($j=$i+1; $j < count($array); $j++) { 
+					 if($array[$j]<$smallest){
+					 	$smallest=$array[$j];
+						$smallest_index=$j;
+						// echo "smallest".$smallest."<br>";
+				         //echo "smallest_index".$smallest_index."<br>";
+					 }
+				}
+				 $temp=$array[$i];
+				 $array[$i]=$smallest;
+				 $array[$smallest_index]=$temp;	 
+			}?>
+			<?php 
+			$time_after=microtime(); 
+			echo "time taken=".($time_after-$time_before)."ms";
+			echo "<br>";
+			?>
 
-			 }
-			// echo "smallest=".$smallest;
-			 echo "index= ".$smallest_index;
-			 //$temp=$array[$i];
-			 //$array[$i]=$smallest;
-			 //$array[$smallest_index]=$temp;
-
-			
-			 //echo "index=".$smallest_index;
-			
-		}
-
-		 
-	 
-
-
-		//var_dump($array);
-	 ?>
+    </div>
+	<div> 
+	    <h2>Selection Sort</h2>
+	    <p><?php
+		 // var_dump($array);
+		 ?>
+		</p>
+	</div>	
 
  
 </body>
